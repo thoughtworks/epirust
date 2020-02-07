64a07abf0c363d06f1cb17073abcfd5eeae3aed4
Akshay Dewan <akshayde@thoughtworks.com>
2020-02-07 16:06:30 +0530

    [DT-MISC] Add options to run with 2 and 5 million agents

--- src/main.rs
+++ src/main.rs
@@ -32,9 +32,15 @@ fn main() {
     } else if count <= 100000 {
         println!("Executing for 100,000 agents");
         sim100_000();
-    } else {
+    } else if count <= 1_000_000 {
         println!("Executing for 1,000,000 agents");
         sim1_000_000();
+    } else if count <= 2_000_000 {
+        println!("Executing for 2,000,000 agents");
+        sim2_000_000();
+    } else {
+        println!("Executing for 5,000,000 agents");
+        sim5_000_000();
     }
     println!("Done");
 }
@@ -104,6 +110,35 @@ fn sim1_000_000() {
     const PUBLIC_TRANSPORT_PERCENTAGE: f64 = 0.2;
     const WORKING_PERCENTAGE: f64 = 0.7;
     const OUTPUT_FILE_NAME: &str = "simulation_1_000_000.csv";
+
+    let mut epidemiology = epidemiology_simulation::Epidemiology::new(GRID_SIZE, NUMBER_OF_AGENTS, PUBLIC_TRANSPORT_PERCENTAGE, WORKING_PERCENTAGE);
+    epidemiology.run(SIMULATION_LIFE_TIME, VACCINATION_TIME, VACCINATION_PERCENTAGE, OUTPUT_FILE_NAME);
+}
+
+fn sim2_000_000() {
+    const GRID_SIZE: i32 = 3550;
+    const NUMBER_OF_AGENTS: i32 = 2_000_000;
+    const SIMULATION_LIFE_TIME: i32 = 10000;
+    const VACCINATION_TIME: i32 = 5000;
+    const VACCINATION_PERCENTAGE: f64 = 0.2;
+    const PUBLIC_TRANSPORT_PERCENTAGE: f64 = 0.2;
+    const WORKING_PERCENTAGE: f64 = 0.7;
+    const OUTPUT_FILE_NAME: &str = "simulation_2_000_000.csv";
+
+    let mut epidemiology = epidemiology_simulation::Epidemiology::new(GRID_SIZE, NUMBER_OF_AGENTS, PUBLIC_TRANSPORT_PERCENTAGE, WORKING_PERCENTAGE);
+    epidemiology.run(SIMULATION_LIFE_TIME, VACCINATION_TIME, VACCINATION_PERCENTAGE, OUTPUT_FILE_NAME);
+}
+
+
+fn sim5_000_000() {
+    const GRID_SIZE: i32 = 5660;
+    const NUMBER_OF_AGENTS: i32 = 5_000_000;
+    const SIMULATION_LIFE_TIME: i32 = 10000;
+    const VACCINATION_TIME: i32 = 5000;
+    const VACCINATION_PERCENTAGE: f64 = 0.2;
+    const PUBLIC_TRANSPORT_PERCENTAGE: f64 = 0.2;
+    const WORKING_PERCENTAGE: f64 = 0.7;
+    const OUTPUT_FILE_NAME: &str = "simulation_5_000_000.csv";
 
     let mut epidemiology = epidemiology_simulation::Epidemiology::new(GRID_SIZE, NUMBER_OF_AGENTS, PUBLIC_TRANSPORT_PERCENTAGE, WORKING_PERCENTAGE);
     epidemiology.run(SIMULATION_LIFE_TIME, VACCINATION_TIME, VACCINATION_PERCENTAGE, OUTPUT_FILE_NAME);

3dd0bd69a6ad794a3bb97501c51e2ba751c5fa3e
dhananzen <dhananjayk.456@gmail.com>
2020-03-10 11:38:42 +0530

    use vega-react to create a line component

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import EpiSimulationParamForm from "./epiSimulationParamForm";
+import VegaLine from './VegaLine';
 function App() {
   return (
     <>
@@ -8,6 +9,7 @@ function App() {
       </nav>
       <div className="container mt-4">
         <EpiSimulationParamForm />
+        {/* <VegaLine></VegaLine> */}
       </div>
     </>
   );

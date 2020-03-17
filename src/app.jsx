6669a6c6f08a7fddc2efb3eeb2ca44621e38b0fb
dhananzen <dhananjayk.456@gmail.com>
2020-03-17 17:59:34 +0530

    Move state up into the app component so as to reset graph on submit

--- src/app.jsx
+++ src/app.jsx
@@ -2,13 +2,84 @@ import React from 'react';
 import ParamterInputForm from './ParameterInputForm';
 import { useState } from 'react';
 import './app.scss';
-import SocketAwareGraph from './SocketAwareGraph';
+
+import { useEffect } from 'react';
+import Dygraph from 'dygraphs';
+
+import io from 'socket.io-client'
 
 function App() {
-  const [displayChartKey, setDisplayChartKey] = useState(null);
+  const [socket, setSocket] = useState(null);
+
+  const [dataBuffer, setDataBuffer] = useState([]);
+  const [graph, setGraph] = useState(null);
+
+  useEffect(() => {
+
+    if (!socket)
+      return
+
+    let buff = [];
+
+    socket.on('epidemicStats', function (messageRaw) {
+      const message = JSON.parse(messageRaw);
+      const { hour, susceptible, infected, quarantined, recovered, deceased } = message;
+
+      buff.push([hour, susceptible, infected, quarantined, recovered, deceased]);
+
+      if (hour % 100 === 0) {
+        setDataBuffer(buffer => {
+          let total = [...buffer, ...buff]
+          buff = [];
+          return total;
+        });
+      }
+    });
+  }, [socket])
+
+  useEffect(() => {
+
+    if (dataBuffer.length === 0)
+      return
+
+    if (!graph) {
+      let graphInstance = new Dygraph(document.getElementById("vis"), dataBuffer, {
+        labels: ["hour", "susceptible", "infected", "quarantined", "recovered", "deceased"]
+      });
+
+      setGraph(graphInstance);
+    }
+    else {
+      graph.updateOptions({ 'file': dataBuffer });
+    }
+
+  }, [graph, dataBuffer])
+
+  function startSimulation() {
+    return fetch("http://localhost:3000/simulation", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+  }
+
+  function startSocket() {
+    setSocket(io('http://localhost:3000/'));
+  }
 
   function handleFormSubmit() {
-    setDisplayChartKey(true);
+    if (socket)
+      socket.close()
+
+    if (graph) {
+      setDataBuffer([]);
+      graph.destroy()
+      setGraph(null);
+    }
+
+    startSimulation()
+      .then(startSocket)
   }
 
   return (
@@ -18,7 +89,7 @@ function App() {
       </nav>
       <div className="container mt-4">
         <ParamterInputForm onSubmit={handleFormSubmit} />
-        {displayChartKey && <SocketAwareGraph num={displayChartKey} />}
+        <div id="vis" style={{ width: "70%", height: "600px" }}></div>
       </div>
     </>
   );

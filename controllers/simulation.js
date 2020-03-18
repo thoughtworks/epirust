ec101c53290d70edd49382250188076a769f8e0a
Akshay Dewan <akshayde@thoughtworks.com>
2020-03-18 16:05:42 +0530

    Change the format of the message sent to kafka, as per new contract

--- controllers/simulation.js
+++ controllers/simulation.js
@@ -29,12 +29,40 @@ router.post('/', (req, res, next) => {
 
 router.post('/init', (req, res, next) => {
   const message = req.body; // { disease_name, grid_size, number_of_agents, simulation_hrs, public_transport_percentage, working_percentage, vaccinate_at, vaccinate_percentage }
+  const simulation_config = {
+    "population": {
+      "Auto": {
+        "number_of_agents": message.number_of_agents,
+        "public_transport_percentage": message.public_transport_percentage,
+        "working_percentage": message.working_percentage
+      }
+    },
+    //TODO Currently hardcoded. These params must come from the UI
+    "disease": {
+      "regular_transmission_start_day": 10,
+      "high_transmission_start_day": 16,
+      "last_day": 22,
+      "regular_transmission_rate": 0.05,
+      "high_transmission_rate": 0.5,
+      "death_rate": 0.2
+    },
+    "grid": message.grid_size,
+    "hours": message.simulation_hrs,
+    "interventions": [
+      {
+        "Vaccinate": {
+          "at_hour": message.vaccinate_at,
+          "percent": message.vaccinate_percentage
+        }
+      }
+    ]
+  };
   const kafkaProducer = new KafkaServices.KafkaProducerService();
 
-  kafkaProducer.send('simulation_requests', message)
+  kafkaProducer.send('simulation_requests', simulation_config);
 
   res.status(200);
   res.send({ status: "Simulation started" });
-})
+});
 
 module.exports = router;

4bf0f785a1c27a5d5eef3eba7ae8ec23725fa3d9
Meenakshi Dhanani <meenad@thoughtworks.com>
2020-03-19 14:36:04 +0530

    Meena | Refactor existing test for parameter form

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders App', () => { //TODO: test child components are rendered, preferably convert to snapshot
+  const { getByText , container} = render(<App />);
+  expect(getByText('EpiViz')).toBeInTheDocument();
 });
--- src/ParameterInputForm.test.js
+++ src/ParameterInputForm.test.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import {render, fireEvent, getByTestId} from '@testing-library/react'
+import { render, fireEvent, getByTestId } from '@testing-library/react'
 import ParameterInputForm from './ParameterInputForm'
 
 test('renders ParameterInputForm with defaults', () => {
-    const {getByText, getByLabelText} = render(<ParameterInputForm/>)
+    const { getByText, getByLabelText } = render(<ParameterInputForm />)
 
     expect(getByLabelText('Number of Agents').value).toBe('10000')
     expect(getByLabelText('Disease Name').value).toBe('small_pox')
@@ -18,7 +18,7 @@ test('renders ParameterInputForm with defaults', () => {
 
 test('invoke onsubmit handler passed on form submit', () => {
     const handleSubmitData = jest.fn()
-    const {getByTestId} = render(<ParameterInputForm onSubmit={handleSubmitData}/>)
+    const { getByTestId } = render(<ParameterInputForm onSubmit={handleSubmitData} />)
 
     fireEvent.submit(getByTestId('simulationForm'))
 
@@ -27,9 +27,19 @@ test('invoke onsubmit handler passed on form submit', () => {
 
 test('make API call on form submit', () => {
     jest.spyOn(global, 'fetch')
-    .mockImplementation(() => Promise.resolve())
+        .mockImplementation(() => Promise.resolve())
     const handleSubmitData = jest.fn()
-    const {getByText, getByTestId} = render(<ParameterInputForm onSubmit={handleSubmitData}/>)
+    const { getByText, getByTestId } = render(<ParameterInputForm onSubmit={handleSubmitData} />)
+    const expectedBody = {
+        "number_of_agents": 10000,
+        "disease_name": "small_pox",
+        "grid_size": 250,
+        "simulation_hrs": 10000,
+        "public_transport_percentage": 0.2,
+        "working_percentage": 0.7,
+        "vaccinate_at": 5000,
+        "vaccinate_percentage": 0.2
+    }
 
     fireEvent.submit(getByTestId('simulationForm'))
 
@@ -37,6 +47,6 @@ test('make API call on form submit', () => {
     expect(global.fetch.mock.calls[0][0]).toBe("http://localhost:3000/simulation/init")
     expect(global.fetch.mock.calls[0][1].method).toBe("POST")
     expect(global.fetch.mock.calls[0][1].body)
-    .toBe(`{"number_of_agents":10000,"disease_name":"small_pox","grid_size":250,"simulation_hrs":10000,"public_transport_percentage":0.2,"working_percentage":0.7,"vaccinate_at":5000,"vaccinate_percentage":0.2}`)
+        .toBe(JSON.stringify(expectedBody))
 })
 

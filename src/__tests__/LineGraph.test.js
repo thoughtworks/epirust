fae1f3cdc04b88dc6e0d3474c2bdc3b98438a74f
dhananzen <dhananjayk.456@gmail.com>
2020-03-20 14:07:30 +0530

    Fix breaking test with the newly added options

--- src/__tests__/LineGraph.test.js
+++ src/__tests__/LineGraph.test.js
@@ -6,7 +6,7 @@ import Dygraph from 'dygraphs'
 jest.mock('dygraphs')
 
 test('should render an empty Graph without calling dygraphs for empty data buffer', () => {
-    const dygraphMockFn = Dygraph.mockImplementation(() => {graph: "mockGraph"})
+    const dygraphMockFn = Dygraph.mockImplementation(() => { graph: "mockGraph" })
     const { getByTestId } = render(<Graph dataBuffer="" />)
 
     expect(getByTestId("visualization")).toBeInTheDocument()
@@ -18,14 +18,20 @@ test('should invoke dygraph when data buffer length is not 0 and graph is null',
     1,10,5,0,0,0
     2,15,6,0,0,0
     3,20,7,0,0,1`
-    const expectedLabels = {
-        "labels": ["hour", "susceptible", "infected", "quarantined", "recovered", "deceased"]
-    } 
-    const dygraphMockFn = Dygraph.mockImplementationOnce(() => {graph: "mockGraph"})
-    render(<Graph dataBuffer={simulationData}/>)
-    
+    const expectedOptions = {
+        "labels": ["hour", "susceptible", "infected", "quarantined", "recovered", "deceased"],
+        legend: 'always',
+        animatedZooms: true,
+        title: 'Time Series Graph',
+        ylabel: 'Number of Agents',
+        xlabel: 'Hours',
+        showRoller: true
+    }
+    const dygraphMockFn = Dygraph.mockImplementationOnce(() => { graph: "mockGraph" })
+    render(<Graph dataBuffer={simulationData} />)
+
     expect(dygraphMockFn).toHaveBeenCalledTimes(1)
-    expect(dygraphMockFn).toHaveBeenCalledWith(expect.anything(), simulationData, expectedLabels)
+    expect(dygraphMockFn).toHaveBeenCalledWith(expect.anything(), simulationData, expectedOptions)
     jest.clearAllMocks()
 })
 
@@ -35,10 +41,10 @@ test('should update dygraph chart when data buffer is not 0 and graph is not nul
     2,15,6,0,0,0
     3,20,7,0,0,1`
     const simulationData2 = "4,30,8,0,0,2"
-    const dygraphMockFn = Dygraph.mockImplementation(() => {})
-    const {rerender} = render(<Graph dataBuffer={simulationData1}/>)
+    const dygraphMockFn = Dygraph.mockImplementation(() => { })
+    const { rerender } = render(<Graph dataBuffer={simulationData1} />)
     expect(dygraphMockFn).toHaveBeenCalledTimes(1)
-    
-    rerender(<Graph dataBuffer={simulationData2}/>)
+
+    rerender(<Graph dataBuffer={simulationData2} />)
     //TODO: test if update method was called with arguments expected
 })

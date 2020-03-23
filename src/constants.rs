d3056f7285ebbff82689d3b34f5883be92dca09d
Akshay Dewan <akshayde@thoughtworks.com>
2020-03-23 16:03:18 +0530

    Add AGPLv3 license
    Adding the IntelliJ files needed for copyright management. Fixing gitignore.
    
    
    Former-commit-id: fd9057b161cd372a589ff737555ba2a181c914c2

--- src/constants.rs
+++ src/constants.rs
@@ -1,3 +1,22 @@
+/*
+ * EpiRust
+ * Copyright (c) 2020  ThoughtWorks, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 pub const HOUSE_AREA_RELATIVE_SIZE: f32 = 0.4;
 pub const TRANSPORT_AREA_RELATIVE_SIZE: f32 = 0.1;
 pub const WORK_AREA_RELATIVE_SIZE: f32 = 0.2;

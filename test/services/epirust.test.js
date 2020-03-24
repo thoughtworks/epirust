d2f5350589b4ac7f030b7dfcb98fa21b751a300a
bawaji94 <bawaji94@gmail.com>
2020-03-24 10:40:02 +0530

    mock execa function to validate if it has been called or not

--- test/services/epirust.test.js
+++ test/services/epirust.test.js
@@ -1,15 +1,17 @@
-const epirust = require('../../services/epirust');
-
 jest.mock('execa');
 
+const execa = require('execa');
+
+const epirust = require('../../services/epirust');
+
 describe('epirust', () => {
-    test('should exec epirust engine', () => {
-        const execa = jest.fn();
+    it('should exec epirust engine', () => {
         const epirustService = new epirust();
 
         epirustService.start(100);
 
+        expect(execa.mock.calls.length).toEqual(1);
+        expect(execa.mock.calls[0]).toEqual(["./external/epirust", [100]]);
         expect(epirustService.engine).toEqual("./external/epirust");
-    //    TODO: Validate execa has been called - Jayanta
     });
 });

8f6cb7e406e237bc38eab44736225c0962bb079c
dhananzen <dhananjayk.456@gmail.com>
2020-03-27 10:26:56 +0530

    DK | Add bootstrap scss files and use only the required ones

--- web/src/index.js
+++ web/src/index.js
@@ -19,9 +19,6 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'bootstrap';
-
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/index.scss';
 import App from './App';
 ReactDOM.render(<App />, document.getElementById('root'));

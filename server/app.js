5bbb647aeec04629629ea2668dfb36607fba15a5
dhananzen <dhananjayk.456@gmail.com>
2020-03-30 18:17:30 +0530

    DK | Fix test breaking due to missing declaration

--- server/app.js
+++ server/app.js
@@ -26,13 +26,13 @@ const router = require('./routes/router');
 const ioInstance = require('./io');
 const app = express();
 const cors = require('cors');
- 
+
 var server = http.createServer(app);
 server.listen(3000);
 server.on('error', onError);
 server.on('listening', onListening);
 ioInstance(server);
-app.use(cors({origin:'http://localhost:3001'}));
+app.use(cors({ origin: 'http://localhost:3001' }));
 app.use(logger('dev'));
 app.use(cors());
 app.use(express.json());
@@ -41,35 +41,35 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', router);
 
 function onError(error) {
-    if (error.syscall !== 'listen') {
-      throw error;
-    }
-  
-    var bind = typeof port === 'string'
-      ? 'Pipe ' + port
-      : 'Port ' + port;
-  
-    // handle specific listen errors with friendly messages
-    switch (error.code) {
-      case 'EACCES':
-        console.error(bind + ' requires elevated privileges');
-        process.exit(1);
-        break;
-      case 'EADDRINUSE':
-        console.error(bind + ' is already in use');
-        process.exit(1);
-        break;
-      default:
-        throw error;
-    }
+  if (error.syscall !== 'listen') {
+    throw error;
   }
-  
-  function onListening() {
-    var addr = server.address();
-    var bind = typeof addr === 'string'
-      ? 'pipe ' + addr
-      : 'port ' + addr.port;
-    debug('Listening on ' + bind);
-  }
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
 
-  module.exports = server
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
+
+module.exports = server

d73cc439c86f2f9431492556e909f645515f0442
dhananzen <dhananjayk.456@gmail.com>
2020-03-31 17:02:11 +0530

    Meena/DK | Add npm-script to run web and server tests

---
+++ web/server/public/precache-manifest.e6495be15757bec6160f3590b588843f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c23dcf7233152d30609ef048748a8ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9688a5191053a49154a0",
+    "url": "/static/css/main.092c1232.chunk.css"
+  },
+  {
+    "revision": "034a12c8687fb2334dcc",
+    "url": "/static/js/2.823561e5.chunk.js"
+  },
+  {
+    "revision": "4236e66adbd85d49283a3582d22f4464",
+    "url": "/static/js/2.823561e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9688a5191053a49154a0",
+    "url": "/static/js/main.3b8c73fd.chunk.js"
+  },
+  {
+    "revision": "6597c35c54cec0f2c71c",
+    "url": "/static/js/runtime-main.1209fa1d.js"
+  }
+]);

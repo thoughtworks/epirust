7158c634ae8a5d45a11d7147b840d2a897b52479
dhananzen <dhananjayk.456@gmail.com>
2020-04-01 15:44:16 +0530

    Revert "Meena/DK | Remove cors, since it not required"
    This reverts commit 79f9c5062f53872ff1618080c36167a92cb3eaec.

--- web/server/app.js
+++ web/server/app.js
@@ -23,6 +23,7 @@ const path = require('path');
 const logger = require('morgan');
 const debug = require('debug')('epirust-server');
 const router = require('./routes/router');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const config = require("./config");
 
@@ -46,7 +47,9 @@ const io = require("socket.io")(server);
 setupIO(io);
 
 
+app.use(cors({origin: 'http://localhost:3001'}));
 app.use(logger('dev'));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));

24f3ea213d72f21cf0be14a67aa5d93310f35e38
dhananzen <dhananjayk.456@gmail.com>
2020-04-01 16:28:08 +0530

    Meena/DK | Add router to the project for rendering different views on different pages

--- web/react-spa/src/App.js
+++ web/react-spa/src/App.js
@@ -17,72 +17,36 @@
  *
  */
 
-import React, {useState} from 'react';
+import React from 'react';
 import Grid from "./grid/Grid";
 import TimeSeries from "./time-series";
 import SimulationConfiguration from "./simulation-configuration";
+import Header from './Header';
 
-function NavItem({name, isActive, onClickHandle}) {
-  const activeClassName = isActive ? "active" : "";
-  return <li className={`nav-item ${activeClassName}`}>
-    <a onClick={onClickHandle} className="nav-link" href="#">{name} </a>
-  </li>;
-}
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+export const Routes = {
+  HOME: "/",
+  TIME_SERIES: "/time-series",
+  GRID: "/grid"
+};
 
 function App() {
-  const views = {
-    HOME: "home",
-    TIME_SERIES: "ts",
-    GRID: "grid"
-  };
-
-  const [viewState, updateViewState] = useState({currentView: views.HOME});
-
-  const onHomeClick = () => updateViewState({currentView: views.HOME});
-
-  const onTimeSeriesClick = () => updateViewState({currentView: views.TIME_SERIES});
-
-  const onGridClick = () => updateViewState({currentView: views.GRID});
-
-  const isHomeActive = () => viewState.currentView === views.HOME;
-
-  const isTimeSeriesActive = () => viewState.currentView === views.TIME_SERIES;
-
-  const isGridActive = () => viewState.currentView === views.GRID;
-
-  function getComponent() {
-    switch (viewState.currentView) {
-      case views.HOME:
-        return <SimulationConfiguration/>;
-      case views.GRID:
-        return <Grid size={50}/>;
-      case views.TIME_SERIES:
-        return <TimeSeries/>;
-      default:
-        return <SimulationConfiguration/>;
-    }
-  }
-
   return (
-    <>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a className="navbar-brand" href="/">EpiViz</a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"/>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav mr-auto">
-            <NavItem name={"Home"} isActive={isHomeActive()} onClickHandle={onHomeClick}/>
-            <NavItem name={"Time Series"} isActive={isTimeSeriesActive()} onClickHandle={onTimeSeriesClick}/>
-            <NavItem name={"Grid Visualization"} isActive={isGridActive()} onClickHandle={onGridClick}/>
-          </ul>
-        </div>
-      </nav>
+    <Router>
+      <Header />
       <div className="container-fluid mt-4">
-        {getComponent()}
-      </div>
-    </>
+        <Switch>
+          <Route exact path={Routes.HOME}><SimulationConfiguration /></Route>
+          <Route path={Routes.GRID}><Grid size={50} /></Route>
+          <Route path={Routes.TIME_SERIES}><TimeSeries /></Route>
+        </Switch>
+      </div >
+    </Router >
   );
 }
 
---
+++ web/react-spa/src/Header.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import { Link, useRouteMatch } from "react-router-dom";
+import { Routes } from './App';
+
+export default function Header() {
+
+    function NavItem({ name, linksTo, activeOnExactMatch = false }) {
+        let match = useRouteMatch({
+            path: linksTo,
+            exact: activeOnExactMatch
+        });
+
+        return (
+            <li className={`nav-item ${match ? "active" : ""}`}>
+                <Link className="nav-link" to={linksTo}>{name}</Link>
+            </li>
+        );
+    }
+
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a className="navbar-brand" href="/">EpiViz</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon" />
+            </button>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav mr-auto">
+
+                    <NavItem name={"Home"} linksTo={Routes.HOME} activeOnExactMatch={true} />
+                    <NavItem name={"Time Series"} linksTo={Routes.TIME_SERIES} />
+                    <NavItem name={"Grid Visualization"} linksTo={Routes.GRID} />
+
+                </ul>
+            </div>
+        </nav>
+    );
+}

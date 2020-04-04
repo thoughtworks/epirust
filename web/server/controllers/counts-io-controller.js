bc202a28bcf054a9b9d2da837b3c7c7df8589440
bawaji94 <bawaji94@gmail.com>
2020-04-04 14:03:56 +0530

    Shabbir | for grid check the grid_consumption_finished flag as end status

--- web/server/controllers/counts-io-controller.js
+++ web/server/controllers/counts-io-controller.js
@@ -17,7 +17,7 @@
  *
  */
 
-const {Simulation, SimulationStatus} = require("../db/models/Simulation");
+const {Simulation} = require("../db/models/Simulation");
 const Count = require("../db/models/Count");
 
 function sendCountsData(socket, lastConsumedHour) {
@@ -35,11 +35,11 @@ function sendCountsData(socket, lastConsumedHour) {
       currentHour = data.hour;
       socket.emit('epidemicStats', data);
     }
-    const findLastRecordQuery = Simulation.findOne({}, {status: 1}, {sort: {'_id': -1}});
+    const findLastRecordQuery = Simulation.findOne({}, {grid_consumption_finished: 1}, {sort: {'_id': -1}});
     const promise = findLastRecordQuery.exec();
 
     await promise.then((simulation) => {
-      if (simulation.status === SimulationStatus.FINISHED) {
+      if (simulation.grid_consumption_finished) {
         socket.emit('epidemicStats', {"simulation_ended": true});
       } else sendCountsData(socket, currentHour);
     })

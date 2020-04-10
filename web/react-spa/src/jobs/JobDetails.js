1431ff9c5f568477aea227f7f6341c8611845b5f
dhananzen <dhananjayk.456@gmail.com>
2020-04-10 17:11:30 +0530

    Meena,DK | Display time series charts with Standard daviation of multiple simulations

--- web/react-spa/src/jobs/JobDetails.js
+++ web/react-spa/src/jobs/JobDetails.js
@@ -18,41 +18,57 @@
  */
 
 import React from "react";
-import TimeSeries from "../time-series";
-import "./job-details.scss"
-import {NavItem} from "../common/NavItem";
-import {Redirect, Route, Switch} from "react-router-dom";
-import GridPage from "../grid";
-import PropTypes from 'prop-types'
 
-export const JobDetails = ({simulationId, details}) => {
+import "./job-details.scss"
+import { NavItem } from "../common/NavItem";
+import { Redirect, Route, Switch } from "react-router-dom";
+import GridPage from "../grid";
+import PropTypes from 'prop-types';
+import { TimeSeriesDeviation, TimeSeries } from "../time-series";
+
+export const JobDetails = ({ simulationId, details }) => {
   const linkPrefix = `/jobs/${simulationId}`;
 
   const isGridEnabled = details && details.config.enable_citizen_state_messages;
 
-  return (
-    <div className="job-details" style={{paddingTop: 4}}>
+  function renderTabs() {
+    return (
       <div className="row">
         <div className="col-8">
           <ul className="nav nav-tabs nav-fill">
-            <NavItem name="Time Series" linksTo={`${linkPrefix}/time-series`} activeOnExactMatch={true}/>
-            {isGridEnabled && <NavItem name="Grid" linksTo={`${linkPrefix}/grid`}/>}
-            <NavItem name="Config" linksTo={`${linkPrefix}/config`}/>
+            <NavItem name="Time Series" linksTo={`${linkPrefix}/time-series`} activeOnExactMatch={true} />
+            <NavItem name="Time Series Deviation" linksTo={`${linkPrefix}/time-series-deviation`} />
+            {isGridEnabled && <NavItem name="Grid" linksTo={`${linkPrefix}/grid`} />}
+            <NavItem name="Config" linksTo={`${linkPrefix}/config`} />
           </ul>
         </div>
         <div className="col-4">
           <h4 className="simulation-id">Simulation: {simulationId}</h4>
         </div>
       </div>
+    );
+  }
+
+  function renderContentForTab() {
+    return (
       <Switch>
-        <Route exact path={"/jobs/:id/time-series"}><TimeSeries simulationId={simulationId}/></Route>
+        <Route exact path={"/jobs/:id/time-series"}><TimeSeries simulationId={simulationId} /></Route>
+        <Route exact path={"/jobs/:id/time-series-deviation"}><TimeSeriesDeviation simulationId={simulationId} /></Route>
         <Route exact path={"/jobs/:id/grid"}>
-          {isGridEnabled ? <GridPage/> : <Redirect to={`/jobs/${simulationId}/time-series`}/>}
+          {isGridEnabled ? <GridPage /> : <Redirect to={`/jobs/${simulationId}/time-series`} />}
         </Route>}
         <Route exact path={"/jobs/:id/config"}>
           {details && <pre>{JSON.stringify(details.config, undefined, 4)}</pre>}
         </Route>
       </Switch>
+    );
+  }
+
+
+  return (
+    <div className="job-details" style={{ paddingTop: 4 }}>
+      {renderTabs()}
+      {renderContentForTab()}
     </div>
   )
 };

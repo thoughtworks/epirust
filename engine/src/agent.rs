2b294ccf72cadcb2535f58c0e668a539a93113d1
Akshay Dewan <akshayde@thoughtworks.com>
2020-04-10 16:16:42 +0530

    Send travellers using kafka

--- engine/src/agent.rs
+++ engine/src/agent.rs
@@ -30,7 +30,7 @@ use serde::{Deserializer, Deserialize, de};
 use serde::de::{Unexpected};
 use crate::listeners::events::counts::Counts;
 
-#[derive(Copy, Clone, PartialEq, Debug)]
+#[derive(Copy, Clone, PartialEq, Debug, Serialize)]
 pub enum State {
     Susceptible {},
     Infected {},
@@ -38,7 +38,7 @@ pub enum State {
     Deceased {},
 }
 
-#[derive(Copy, Clone, PartialEq, Debug)]
+#[derive(Copy, Clone, PartialEq, Debug, Serialize)]
 pub struct StateMachine {
     pub state: State,
     infection_day: i32,
@@ -88,7 +88,7 @@ fn bool_from_string<'de, D>(deserializer: D) -> Result<bool, D::Error>
     }
 }
 
-#[derive(Copy, Clone, Debug)]
+#[derive(Copy, Clone, Debug, Serialize)]
 pub struct Citizen {
     pub id: i32,
     immunity: i32,

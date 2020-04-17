d2f78a4feb1987ae60afb122c078802dae649162
Swapnil Khandekar <swapnikh@thoughtworks.com>
2020-04-17 17:46:53 +0530

    Swapnil | Add test to make sure that duplicate interventions are not created for a count

--- web/server/test/db/services/CountService.test.js
+++ web/server/test/db/services/CountService.test.js
@@ -22,24 +22,50 @@ const CountService = require("../../../db/services/CountService");
 const {Count} = require('../../../db/models/Count');
 
 describe('CountService', function () {
-  beforeAll(async () => await dbHandler.connect());
-  afterEach(async () => await dbHandler.clearDatabase());
-  afterAll(async () => await dbHandler.closeDatabase());
-
   describe('addIntervention', function () {
     it('should add intervention to the count', async function () {
-      const simulationId= 12434;
-      const interventionName = "test-intervention";
-      const interventionData = {status: "done"};
-      const intervention = {hour: 12, intervention: interventionName, data: interventionData};
+      const simulationId = randomId();
+      const interventionObject = buildTestIntervention();
       await new Count({simulation_id: simulationId, hour: 12}).save();
 
-      await CountService.addIntervention(simulationId, intervention);
+      await CountService.addIntervention(simulationId, interventionObject);
 
       const count = await Count.findOne({simulation_id: simulationId, hour: 12}).exec();
       const receivedCount = count.toObject();
       expect(receivedCount.interventions).toHaveLength(1);
-      expect(receivedCount.interventions[0]).toMatchObject({intervention: interventionName, data: interventionData});
+      expect(receivedCount.interventions[0]).toMatchObject({
+        intervention: interventionObject.intervention,
+        data: interventionObject.data
+      });
+    });
+
+    it('should not duplicate interventions if same added twice', async function () {
+      const simulationId = randomId();
+      const interventionObject = buildTestIntervention();
+      await new Count({simulation_id: simulationId, hour: 12}).save();
+
+      await CountService.addIntervention(simulationId, interventionObject);
+      await CountService.addIntervention(simulationId, interventionObject);
+
+      const count = await Count.findOne({simulation_id: simulationId, hour: 12}).exec();
+      const receivedCount = count.toObject();
+      expect(receivedCount.interventions).toHaveLength(1);
+      expect(receivedCount.interventions[0]).toMatchObject({
+        intervention: interventionObject.intervention,
+        data: interventionObject.data
+      });
     });
   });
+
+  beforeAll(async () => await dbHandler.connect());
+  afterEach(async () => await dbHandler.clearDatabase());
+  afterAll(async () => await dbHandler.closeDatabase());
+
+  const randomId = () => Math.random();
+
+  const buildTestIntervention = () => {
+    const interventionName = "test-intervention";
+    const interventionData = {status: "done"};
+    return {hour: 12, intervention: interventionName, data: interventionData};
+  };
 });

92d3a39f7b3da4d4927f065010baee96977de4f3
Akshay Dewan <akshayde@thoughtworks.com>
2020-05-11 16:31:39 +0530

    Update disease dynamics

--- web/react-spa/src/simulation-configuration/diseases.js
+++ web/react-spa/src/simulation-configuration/diseases.js
@@ -26,16 +26,16 @@ export const diseases = {
       "pre_symptomatic_duration": 0
     },
     "covid_19": {
-      "label": "COVID-19", 
-      "regular_transmission_start_day": 5,
-      "high_transmission_start_day": 20,
-      "last_day": 40,
-      "regular_transmission_rate": 0.025,
-      "high_transmission_rate": 0.25,
-      "death_rate": 0.2,
+      "label": "COVID-19",
+      "death_rate": 0.035,
       "percentage_asymptomatic_population": 0.3,
-      "percentage_severe_infected_population": 0.3,
       "exposed_duration": 48,
-      "pre_symptomatic_duration": 48
+      "last_day": 26,
+      "regular_transmission_rate": 0.25,
+      "pre_symptomatic_duration": 48,
+      "percentage_severe_infected_population": 0.3,
+      "high_transmission_start_day": 6,
+      "high_transmission_rate": 0.25,
+      "regular_transmission_start_day": 5
     }
   }

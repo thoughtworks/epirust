8ef60a36e0d5d3870a7b88e498e32765642eeda3
Harshal Hayatnagarkar <harshalh@thoughtworks.com>
2020-07-29 20:13:10 +0530

    Removed experiements scripts belonging to an older format.

--- engine/experiments/01.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-for i in {1..5}; do cargo run --release -- -a 10000 -d sars; done
-
-for i in {1..5}; do cargo run --release -- -a 100000 -d sars; done
-
-for i in {1..5}; do cargo run --release -- -a 10000 -d covid_19; done
-
-for i in {1..5}; do cargo run --release -- -a 100000 -d covid_19; done
--- engine/experiments/05.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-for i in {1..3}; do cargo run --release -- -a 1000000 -d covid_19; done

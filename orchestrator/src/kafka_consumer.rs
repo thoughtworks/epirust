b005a0777a816c99fd9ea6fc75899e627edcff74
sapanaKale <sapanakale98@gmail.com>
2022-08-10 12:36:58 +0530

    [Shubham | Sapana] : Add config to allow consuming messages greater than 1MB

--- orchestrator/src/kafka_consumer.rs
+++ orchestrator/src/kafka_consumer.rs
@@ -13,6 +13,7 @@ impl KafkaConsumer {
         let consumer: StreamConsumer = ClientConfig::new()
             .set("bootstrap.servers", kafka_url.as_str())
             .set("group.id", "orchestrator")
+            .set("message.max.bytes", "104857600")
             .set("auto.offset.reset", "earliest")
             .create()
             .expect("Consumer creation failed");
